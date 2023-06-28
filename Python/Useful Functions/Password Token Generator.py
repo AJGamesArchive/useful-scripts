3e6f429aa461d092528d9751454e848841ed2821
AJGamesArchive <68122156+AJGamesArchive@users.noreply.github.com>
2023-06-28 03:08:12 +0100

    Updated Python Password Generator

--- Python/Useful Functions/Password Token Generator.py
+++ Python/Useful Functions/Password Token Generator.py
@@ -1,25 +1,20 @@
 import random
 import string
 
-alphabet = string.ascii_letters
-digits = string.digits
-symbols = ["!", "?", "%", "&"]
-
-choice = "12"
-
-token = ""
-
-for i in range(100):
-  selection = random.choice(choice)
-  match selection:
-    case "1":
+def passwordGenerator():
+  alphabet = string.ascii_letters
+  digits = string.digits
+  symbols = ["!", "?", "%", "&"]
+  choice = "12"
+  token = ""
+  for i in range(20):
+    selection = random.choice(choice)
+    if selection == "1":
       char = random.choice(alphabet)
-    case "2":
+    if selection == "2":
       char = random.choice(digits)
-    case "3":
+    if selection == "3":
       char = random.choice(symbols)
-  token = token + char
+    token = token + char
 
-print(token)
-
-#TODO Turn into a single function
+print(passwordGenerator)
